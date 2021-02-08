0b91ea12015507e73898fb8e7191b028b51635f0
abderrahim <info.abderrahimtijani@gmail.com>
2021-02-08 14:26:40 +0100

    add swiper slider

--- src/index.js
+++ src/index.js
@@ -34,3 +34,65 @@ function scrollFunction() {
     }
 }
 
+  
+var swiper = new Swiper('.swiper1', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 1000,
+  },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+});
+
+
+/* ====== for testimonials from swiper ======== */
+
+var mySwiper = new Swiper('.swiper2', {
+  // autoplay
+  autoplay: {
+    delay: 5000,
+  },
+
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+ 
+   // pagination
+   pagination: {
+     el: '.swiper-pagination2',
+     clickable: true,
+   },
+
+});
+
+/* ======= for testimonials from swiper ======= */
