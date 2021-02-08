651f3000c7738ec9b36ec0bc84b80c5580d16eaa
abderrahim <info.abderrahimtijani@gmail.com>
2021-02-08 14:34:53 +0100

    add GSAP

--- src/index.js
+++ src/index.js
@@ -96,3 +96,82 @@ var mySwiper = new Swiper('.swiper2', {
 });
 
 /* ======= for testimonials from swiper ======= */
+
+
+
+
+//------------ GSAP ------------ //
+
+// or all tools are exported from the "all" file (excluding bonus plugins):
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+// hero 
+
+gsap.from(".header__img", { duration: 1, opacity: 0, x: 100});
+gsap.from(".hero-item", { delay:1, duration: 1, opacity: 0, y: 50, stagger: 0.4 });
+
+
+// services
+gsap.from(".services__item", { 
+  scrollTrigger: {
+      trigger: ".services__item",
+      toggleActions: "restart none none none"
+  },
+ duration: 1.5,
+  opacity: 0,
+  stagger: 0.2
+});
+
+
+// features
+
+gsap.from(".features__details__img", { 
+    scrollTrigger: {
+        trigger: ".features__details__img",
+        toggleActions: "restart none none none"
+    },
+   duration: 1.5,
+   ease: "circ.out", 
+   x: -300
+  });
+
+
+gsap.from(".FDC__item", { 
+    scrollTrigger: {
+        trigger: ".FDC__item",
+        toggleActions: "restart none none none"
+    },
+    delay: 1, duration: 1, opacity: 0, y: 50, stagger: 0.4
+  });
+
+
+
+// pricing
+
+gsap.from(".pricing__options__item", { 
+  scrollTrigger: {
+      trigger: ".pricing__options__item",
+      toggleActions: "restart none none none"
+  },
+  duration: 1.5,
+  opacity: 0,
+  y: 200,
+  stagger: 0.2
+});
+
+// CTA 
+gsap.from(".CTA__img", { scrollTrigger: {
+  trigger: ".CTA__img",
+  toggleActions: "restart none none none"
+},
+duration: 1, opacity: 0, x: 100});
+
+gsap.from(".CTA-item", {
+  scrollTrigger: {
+    trigger: ".CTA-item",
+    toggleActions: "restart none none none"
+},
+delay: 1, duration: 1, opacity: 0, y: 50, stagger: 0.4 });
